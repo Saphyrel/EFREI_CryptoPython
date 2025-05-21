c7f6c345173becba983839e50913ecc34550ab7d
Saphyrel <92301135+Saphyrel@users.noreply.github.com>
2025-05-21 10:46:59 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -21,6 +21,12 @@ def encryptage(valeur):
     valeur_bytes = valeur.encode()  # Conversion str -> bytes
     token = f.encrypt(valeur_bytes)  # Encrypt la valeur
     return f"Valeur encryptée : {token.decode()}"  # Retourne le token en str
-                                                                                                                                                     
+
+@app.route('/decrypt/<string:valeur>')
+def decryptage(valeur):
+    valeur_bytes = valeur.encode()
+    token = f.decrypt(valeur_bytes)
+    return f"Valeur décryptée : {token.decode()}"
+
 if __name__ == "__main__":
   app.run(debug=True)
